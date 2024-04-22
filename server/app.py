8dd94b06bc6860daed4af3f22e046b4fc515e8ef
Itzkirimi <109696386+Itzkirimi@users.noreply.github.com>
2024-04-22 14:41:17 +0300

    Add registration and login functionality with JWT-based authentication
    This commit adds the ability to register and log in users with Flask-JWT-Extended. A new route for registration has been added, which creates a new user in the shop

--- server/app.py
+++ server/app.py
@@ -1,17 +1,22 @@
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db, User, Product
 from flask_cors import CORS
-
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity,create_access_token
+import datetime
+from bcrypt import checkpw
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["JWT_SECRET_KEY"] = 'your-secret-key'  
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
+
+jwt = JWTManager(app)
 
 db.init_app(app)
 api = Api(app)
@@ -56,6 +61,28 @@ class Products(Resource):
     
 api.add_resource(Products, '/products')
 
+
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    new_user = User(username=data['username'], gmail=data['gmail'])
+    new_user.set_password(data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'}), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user:
+        token = create_access_token(identity=user.id, expires_delta=datetime.timedelta(days=1))
+        return jsonify({'token': token}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
 
 
 
